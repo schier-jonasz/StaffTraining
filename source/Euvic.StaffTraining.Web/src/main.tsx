d1026c50b189aaff3c5a2713f7e514cc206c0e98
Jonasz Schier <schier-jonasz@gmail.com>
2022-10-17 21:15:01 +0200

    added react router

--- source/Euvic.StaffTraining.Web/src/main.tsx
+++ source/Euvic.StaffTraining.Web/src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { 
+  createBrowserRouter,
+  RouterProvider,
+  Route, 
+} from 'react-router-dom';
+import ErrorPage from './pages/errorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/attendees',
+    element: <h1>Hello attendees</h1>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
